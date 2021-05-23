ffd5f3e2e24388a61d9462ed1da3e558741f1539
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-23 23:52:35 +0530

    commit

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import { Carousel } from "react-responsive-carousel";
+
+function Banner() {
+  return (
+    <div className="relative ">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
@@ -7,9 +8,15 @@ export default function Home() {
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-      
 
-      <Header/>
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/*Banner */}
+        <Banner />
+
+        {/*Product Feed */}
+      </main>
     </div>
   );
 }
