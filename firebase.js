dc1f0317870da185dbf876b1fd36d3a1e03f536b
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-24 20:15:04 +0530

    commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOZ2VdZCj6W_WH7tiSYMKtGmM_jp_Mlpw",
+  authDomain: "nextjs-4e1b6.firebaseapp.com",
+  projectId: "nextjs-4e1b6",
+  storageBucket: "nextjs-4e1b6.appspot.com",
+  messagingSenderId: "635962100100",
+  appId: "1:635962100100:web:9186603f3b30b24bbff5d1",
+};
