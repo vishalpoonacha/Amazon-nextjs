6dd8375c601f98ee48fd248abffc84900848ccea
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-24 13:03:32 +0530

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,10 @@
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Banner() {
   return (
     <div className="relative ">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
       <Carousel
         autoPlay
         infiniteLoop
@@ -11,9 +13,15 @@ function Banner() {
         showThumbs={false}
         interval={5000}
       >
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
       </Carousel>
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100 ">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -16,7 +17,21 @@ export default function Home() {
         <Banner />
 
         {/*Product Feed */}
+        <ProductFeed products={products} />
       </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
+//GET >>> https://fakestoreapi.com/products
