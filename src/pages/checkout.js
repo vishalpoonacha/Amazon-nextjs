3a18ea867fe8d4cc182c6d397aca7e4bf8b5c359
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-26 12:37:20 +0530

    commit

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,11 +5,34 @@ import Header from "../components/Header";
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/client";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
   const [session] = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    //Call the backend to create the checkout session..
+    const CheckoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+
+    //Redirect user/customer to Stripe checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: CheckoutSession.data.id,
+    });
+
+    if (result.error) {
+      alert(result.error.message);
+    }
+  };
   return (
     <div className="bg-gray-100">
       <Header />
@@ -58,6 +81,8 @@ function Checkout() {
               </h2>
 
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
